ab0effe14fbdd07e8fb7f8ef9c3b5be835cdb195
kozhydlo <markkozhydlo@gmail.com>
2024-10-02 11:43:56 +0300

    все готово треба зробити верстку бо зараз все в консолі

--- src/store/WeatherSlice.js
+++ src/store/WeatherSlice.js
@@ -1,9 +1,28 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const getWeather = createAsyncThunk(
     'weather/getWeather',
-    async () => {
-        console.log('hello world')
+    async (CityName) => {
+        try {
+            // Getting coordinates by city name
+            let coordinates = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${CityName}&count=1&language=en&format=json`)
+            let {latitude, longitude} = coordinates.data.results[0]
+            // Getting weather data
+            let response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`)
+            let {temperature_2m} = response.data.hourly;
+            let temperature = []
+
+            for (let i = 6; i <= 18; i += 3) {
+                temperature.push({
+                    time: `${i}:00`,
+                    temp: temperature_2m[i],
+                })
+            }
+            return temperature;
+        } catch (e) {
+            throw new Error('Check the city name')
+        }
     }
 )
 
@@ -12,10 +31,19 @@ const weatherSlice = createSlice({
     initialState: {
         data: null
     },
-    reducers: {
-        setWeather: (state, action) => {
-            state.weather = action.payload
-        }
+    extraReducers (builder) {
+        builder.addCase(getWeather.fulfilled, (state, action) => {
+            // state.data = action.payload
+            console.log('Finish loading...')
+            console.log(action.payload)
+        })
+        builder.addCase(getWeather.pending, (state, action) => {
+            console.log('Start loading...')
+        })
+        builder.addCase(getWeather.rejected, (state, action) => {
+            console.log('Finish loading...')
+            console.log(action.error.message)
+        })
     }
 })
 
